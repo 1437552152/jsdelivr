86cb5f0903968c66077577c676e06c7cb8df9fcd
yyf <38172479+1437552152@users.noreply.github.com>
2023-06-11 18:21:15 +0800

    Update classification.js

--- zhenda/assets/js/product/classification.js
+++ zhenda/assets/js/product/classification.js
@@ -1,1 +1,42 @@
 
+$(function () {
+  $('.btn-qrcode').click(function () {
+    $('.popup-qrcode').addClass('active')
+  })
+
+  $('.popup-qrcode').click(function () {
+    $('.popup-qrcode').removeClass('active')
+  })
+
+  $('.btn-search').click(function () {
+    const hasUnfold = $('#header').hasClass('unfold');
+    if (hasUnfold) {
+      $('#header').removeClass('unfold')
+    } else {
+      $('#header').addClass('unfold')
+    }
+  })
+
+  $('.btn-menu').click(function () {
+    $('#mb-menu').addClass('unfold')
+  })
+
+  $('#mb-menu .btn-close').click(function () {
+    $('#mb-menu').removeClass('unfold')
+  })
+  $('.menu-mask').click(function () {
+    $('#mb-menu').removeClass('unfold')
+  })
+
+  var toggleSearch = $('[data-class="searchClose"]');
+  toggleSearch && toggleSearch.click(function () {
+    $('#header').removeClass('unfold')
+  })
+
+  $('#gotop').click(function () {
+    $('body,html').animate({
+      'scrollTop': '0'
+    }, 600);
+  })
+
+})
